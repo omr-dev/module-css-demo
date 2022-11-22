fd8a6f43c62a2151e8c3f223ba3736983eb2b3ba
omer <oemer.peynirci@ontavio.de>
2022-11-22 14:44:12 +0100

    solution added

--- src/components/A/A.tsx
+++ src/components/A/A.tsx
@@ -1,5 +1,12 @@
-import './a.css'
-const A=()=>{
-    return <h2 className="title">A</h2>
-}
+// import './a.css'
+import styles from './a.module.css';
+
+
+const A = () => {
+
+
+    // return <h2 className="title">A</h2>
+
+    return <h2 className={styles.title}>A</h2>;
+};
 export default A;
--- src/components/B/B.tsx
+++ src/components/B/B.tsx
@@ -1,5 +1,9 @@
-import './b.css'
+// import './b.css'
+import styles from './b.module.css'
+
 const B=()=>{
-    return <h2 className="title">B</h2>
+    // return <h2 className="title">B</h2>
+    return <h2 className={styles.title}>B</h2>
+
 }
 export default B;
